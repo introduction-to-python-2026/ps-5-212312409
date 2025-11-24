d6c9cec436abde80b357f44637287dbcc122c3ec
212312409 <Shani.aviad1@gmail.com>
2025-11-24 16:55:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sting_utils as string_u, equation_utils as equation_u 
+import string_utils as string_u, equation_utils as equation_u 
 
 def balance_reaction(reaction): #"Fe2O3 + H2 -> Fe + H2O"
     
