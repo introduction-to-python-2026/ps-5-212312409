069a24f77cd60053ee3a5750683edb5c54214a1b
212312409 <Shani.aviad1@gmail.com>
2025-11-24 17:00:57 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -8,13 +8,13 @@ def split_befor_uppercases(formula):
         
     while end < len(formula):
         if formula[end].isupper():
-        elements_lst.append(formula[start:end])
+            elements_lst.append(formula[start:end])
             start = end
         end+=1
         
     elements_lst.append(formula[start:])
 
-    return element_lst
+    return elements_lst
 
 def split_at_digit(formula):
     for char_index, char in enumerate(formula):
@@ -24,18 +24,19 @@ def split_at_digit(formula):
         
 def count_atoms_in_molecule(molecular_formula):
     atom_dict = {}
-    for atom in split_by_capitals(molecular_formula):
-        atom_name, atom_count = split_at_number(atom)
-        atom_dict[atom_name] = atom_count
-    return atom_dict
-
+    for atom in split_befor_uppercases(molecular_formula):
+        atom_name, atom_count = split_at_digit(atom)
+        atom_dict[atom_name] = atom_count_dict.get(atom_name, 0) + atom_count
+    return atom_count_dict
 
 def parse_chemical_reaction(reaction_equation):
+    
     reaction_equation = reaction_equation.replace(" ", "")  # Remove spaces for easier parsing
     reactants, products = reaction_equation.split("->")
     return reactants.split("+"), products.split("+")
 
 def count_atoms_in_reaction(molecules_list):
+    
     molecules_atoms_count = []
     for molecule in molecules_list:
         molecules_atoms_count.append(count_atoms_in_molecule(molecule))
